ecb29a27a4eca394959c9fa85d6aec4b0199f934
Sabi <szigyi.szogyi@gmail.com>
2023-02-17 20:01:17 +0000

    add loggings at the beginning of the app

--- src/auto-zoom-controller/main.py
+++ src/auto-zoom-controller/main.py
@@ -18,6 +18,10 @@ def run(interval_in_seconds, length_in_minutes):
     number_of_total_activations = Logic.calculate_number_of_activations(interval_in_seconds, length_in_minutes)
     activated = 0
 
+    print("Activations: ", number_of_total_activations)
+    print("Total Turns: ", number_of_total_turns)
+    print("Turns: ", turns)
+
     try:
         motor = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
         GPIO.output(12, 0)  # set to low as documentation requires it
