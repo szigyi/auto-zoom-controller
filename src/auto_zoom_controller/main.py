02e98a58dcdf17de743aeb3fd2d13c02d25e60d2
szigyi <szigyi.szogyi@gmail.com>
2023-03-10 12:48:38 +0000

    let the motor turn before scheduler reruns the job

--- src/auto_zoom_controller/main.py
+++ src/auto_zoom_controller/main.py
@@ -12,7 +12,6 @@ def run(interval_in_seconds, length_in_minutes):
     number_of_total_turns = 4000  # lens specific
     turns = Logic.calculate_number_of_turns(number_of_total_turns, interval_in_seconds, length_in_minutes)
     number_of_total_activations = Logic.calculate_number_of_activations(interval_in_seconds, length_in_minutes)
-    activated = 0
 
     print("Activations: ", number_of_total_activations)
     print("Total Turns: ", number_of_total_turns)
@@ -24,9 +23,9 @@ def run(interval_in_seconds, length_in_minutes):
 
         schedule.every(interval_in_seconds).seconds.do(auto_zoom.job)
 
-        while activated <= number_of_total_activations:
+        while auto_zoom.activations() <= number_of_total_activations:
             schedule.run_pending()
-            time.sleep(1)
+            time.sleep(2)
 
         schedule.clear()
         auto_zoom.stop()
@@ -35,13 +34,13 @@ def run(interval_in_seconds, length_in_minutes):
     except Exception as e:
         print("Error {0}".format(str(e.args[0])).encode("utf-8"))
         schedule.clear()
-        print("\nMotor stop")
+        print("Motor stop")
         auto_zoom.stop()
         GPIO.cleanup()
         exit()
     except:
         schedule.clear()
-        print("\nMotor stop")
+        print("Motor stop")
         auto_zoom.stop()
         GPIO.cleanup()
         exit()
@@ -49,5 +48,5 @@ def run(interval_in_seconds, length_in_minutes):
 
 if __name__ == '__main__':
     interval_in_seconds = 5
-    length_in_minutes = 5
+    length_in_minutes = 1
     run(interval_in_seconds, length_in_minutes)
