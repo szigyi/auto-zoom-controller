27ec0d20970a65433bee24e1585f04704e975e1c
szigyi <szigyi.szogyi@gmail.com>
2023-03-13 21:50:29 +0000

    use absolute time for shceduling it is more predictable and precise there is no drifting and additive error of time

--- src/auto_zoom_controller/main.py
+++ src/auto_zoom_controller/main.py
@@ -1,14 +1,19 @@
 import RPi.GPIO as GPIO
 import Logic
-from DRV8825 import DRV8825
-from DRV8825_Helper import Stepper, Direction
-import schedule
+from datetime import datetime
 import time
 
 from auto_zoom_controller.AutoZoom import AutoZoom
 
 
-def run(number_of_total_turns, interval_in_seconds, length_of_transition_in_minutes):
+def check_time(start_time, interval_in_seconds):
+    current_time = datetime.now()
+    difference = current_time - start_time
+    diff_in_seconds = int(difference.total_seconds())
+    return diff_in_seconds % interval_in_seconds == 0
+
+
+def run(start_time, number_of_total_turns, interval_in_seconds, length_of_transition_in_minutes):
     turns = Logic.calculate_number_of_turns(number_of_total_turns, interval_in_seconds, length_of_transition_in_minutes)
     number_of_total_activations = Logic.calculate_number_of_activations(interval_in_seconds, length_of_transition_in_minutes)
 
@@ -20,24 +25,22 @@ def run(number_of_total_turns, interval_in_seconds, length_of_transition_in_minu
         auto_zoom = AutoZoom(turns)
         GPIO.output(12, 0)  # set to low as documentation requires it
 
-        schedule.every(interval_in_seconds).seconds.do(auto_zoom.job)
-
         while auto_zoom.activations() < number_of_total_activations:
-            schedule.run_pending()
-            time.sleep(2)
+            is_it_the_time = check_time(start_time, 5)
+            if (is_it_the_time):
+                auto_zoom.job()
+                time.sleep(2) # let the motor turn and the time pass so no more activation in this second
+            time.sleep(0.3)
 
-        schedule.clear()
         auto_zoom.stop()
         GPIO.cleanup()
     except Exception as e:
         print("Error {0}".format(str(e.args[0])).encode("utf-8"))
-        schedule.clear()
         print("Motor stop")
         auto_zoom.stop()
         GPIO.cleanup()
         exit(1)
     except:
-        schedule.clear()
         print("Motor stop")
         auto_zoom.stop()
         GPIO.cleanup()
@@ -50,4 +53,5 @@ if __name__ == '__main__':
     number_of_total_turns = 27200  # lens specific - 24-240mm Sony G
     interval_in_seconds = 5
     length_of_transition_in_minutes = 10
-    run(number_of_total_turns, interval_in_seconds, length_of_transition_in_minutes)
+    start_time = datetime.now()
+    run(start_time, number_of_total_turns, interval_in_seconds, length_of_transition_in_minutes)
